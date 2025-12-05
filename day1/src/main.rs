8f15dbe2420f81e971d08703536d444d93e924de
Calle20 <pascal.scheuner@web.de>
2025-12-05 21:29:06 +0100

    init and day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+    println!("Day 1");
+
+    //read from input.txt
+    let input = std::fs::read_to_string("input.txt").expect("Failed to read input file");
+    let lines: Vec<&str> = input.lines().collect();
+
+    let mut current_position = 50;
+    let mut zero_count = 0;
+
+    for line in lines{
+        let char = line.chars().next().unwrap();
+        let mut distance = line[1..].parse::<i32>().unwrap();
+
+        println!("Char: {}, Distance: {}", char, distance);
+
+        match char {
+            'L' => {
+                for i in 0..distance {
+                    current_position -= 1;
+                    if current_position < 0 {
+                        current_position = 99;
+                    }
+                    if current_position == 0 {
+                        zero_count += 1;
+                    }
+                }
+            },
+            'R' => 
+            {
+                for i in 0..distance {
+                    current_position += 1;
+                    if current_position > 99 {
+                        current_position = 0;
+                    }
+                    if current_position == 0 {
+                        zero_count += 1;
+                    }
+                }
+            },
+            _ => panic!("Invalid character in input"),
+        }
+        //p1 
+        println!("Current Position: {}, Zero Count: {}", current_position, zero_count);
+    }
+    println!("Current Position: {}, Zero Count: {}", current_position, zero_count);  
+}
